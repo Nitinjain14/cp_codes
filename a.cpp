279cf7e267ba187b169b25ff0e056b75b9aaab7d
Nitin Jain <anchalia.nitin@gmail.com>
2025-07-18 15:08:57 +0530

    A. Soldier and Bananas-800

--- a.cpp
+++ a.cpp
@@ -2,22 +2,11 @@
 using namespace std;
  
 void solve(){
-    int n,k;
-    cin>>n>>k;
+    int k,n,w;
+    cin>>k>>n>>w;
+    int x=w*(w+1);
 
-    while(n>0 && n%10<k){
-        if(n%10!=0 && n%10<k){
-            k-=(n%10);
-            n-=(n%10);
-        }
-        else if(n%10==0){
-            n=n/10;
-            k-=1;
-        }
-    }
-    if(n%10>=k){
-        cout<<n-k;
-    }
+    cout<<abs(((x/2)*k)-n);
 }
  
 int main(){
