365eb2963410a7992589a016e85f8d279af4f9ea
Nitin Jain <anchalia.nitin@gmail.com>
2025-07-18 17:19:45 +0530

    2D Traveling-1100 optimized code by seeing ans

--- a.cpp
+++ a.cpp
@@ -1,32 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
- 
-void solve(){
-    int k,n,w;
-    cin>>k>>n>>w;
-    int x=w*(w+1);
 
-    cout<<abs(((x/2)*k)-n);
+void solve() {
+    long long n, k, a, b;
+    cin >> n >> k >> a >> b;
+
+    vector<pair<long long, long long>> coords(n);
+    for (int i = 0; i < n; i++) {
+        cin >> coords[i].first >> coords[i].second;
+    }
+
+    long long cost_org = llabs(coords[a - 1].first - coords[b - 1].first) +
+                         llabs(coords[a - 1].second - coords[b - 1].second);
+
+    long long x = LLONG_MAX/2, y = LLONG_MAX/2;
+    for (int i = 0; i < k; i++) {
+        x = min(x, llabs(coords[a - 1].first - coords[i].first) + llabs(coords[a - 1].second - coords[i].second));
+        y = min(y, llabs(coords[b - 1].first - coords[i].first) + llabs(coords[b - 1].second - coords[i].second));
+    }
+
+    cout << min(cost_org, x + y) << '\n';
 }
- 
-int main(){
- 
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
- 
-    #ifndef ONLINE_JUDGE
-        freopen("./outputs/input.txt", "r", stdin);
-        freopen("./outputs/output.txt", "w", stdout);
-    #endif
- 
-    //int t; cin>>t;while(t--)
-    solve();
- 
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("./outputs/input.txt", "r", stdin);
+    freopen("./outputs/output.txt", "w", stdout);
+#endif
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
     return 0;
 }
- 
-// Explanation 
-/*
- 
-*/
